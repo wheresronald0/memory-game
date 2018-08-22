0666e1772f965749f67e01611b90bd51ddb77ad1
Ron M <wheresronald0@gmail.com>
2018-08-22 12:43:34 -0400

    created game logic

---
+++ game/JS/main.js
@@ -0,0 +1,7 @@
+console.log("up and running!");
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+console.log('User Flipped ' + cardOne);
+console.log('User Flipped ' + cardThree);
