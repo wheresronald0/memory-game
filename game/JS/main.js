0b4e2cca584efad72aaf474a0a8d821416cb84bd
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-08-23 16:04:13 -0400

    created game logic

--- game/JS/main.js
+++ game/JS/main.js
@@ -1,3 +1,4 @@
+/*
 console.log("up and running!");
 var cardOne = 'queen';
 var cardTwo = 'queen';
@@ -5,3 +6,29 @@ var cardThree = 'king';
 var cardFour = 'king';
 console.log('User Flipped ' + cardOne);
 console.log('User Flipped ' + cardThree);
+*/
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[3];
+var cardThree = cards[2];
+var cardFour = cards[1];
+
+cardInPlay.push(cardOne);
+cardInPlay.push(cardTwo);
+/*
+cardInPlay.push(cardThree);
+cardInPlay.push(cardFour);
+*/
+
+if (cardInPlay.length === 2) {
+	cardInPlay[0] === cardInPlay[1];
+	alert("You found a match!");	
+} else {
+	alert("Sorry, try again!");
+}
+
+
+//console.log('User Flipped ' + cardTwo);
