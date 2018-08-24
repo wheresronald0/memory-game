cf4d6d7d991d84a837056bc95878349e42df516f
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-08-24 10:27:05 -0400

    added more game logic

--- game/JS/main.js
+++ game/JS/main.js
@@ -8,7 +8,30 @@ console.log('User Flipped ' + cardOne);
 console.log('User Flipped ' + cardThree);
 */
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+		{
+			rank : "queen",	
+			suit: "hearts",
+			cardImage: "images/queen-of-hearts.png",
+		},
+		{
+			rank : "queen",	
+			suit: "diamonds",	
+			cardImage: "images/queen-of-diamonds.png",
+		},
+		{
+			rank : "king",	
+			suit: "hearts",	
+			cardImage: "images/king-of-hearts.png",
+		},
+		{
+			rank : "king",	
+			suit: "diamonds",	
+			cardImage: "images/king-of-diamonds.png",
+		},
+
+]
+
 var cardInPlay = [];//is only 2 cards at a time 
 
 /*
@@ -24,7 +47,7 @@ var checkForMath = funtion () {
 
 
 var flipCard = function (cardId) { //to store all steps that should happen when the user flips a card.//
-	cardInPlay.push(cards[cardId]);
+	cardInPlay.push(cards[cardId].rank);
 
 		if (cardInPlay.length === 2) {
 
@@ -34,7 +57,9 @@ var flipCard = function (cardId) { //to store all steps that should happen when 
 			alert("Sorry, try again!");
 		}
 	}
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 
 
