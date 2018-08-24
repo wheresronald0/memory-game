564e113f0d35b8de5931883c129a39283fc8fbb1
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-08-24 08:09:08 -0400

    created more logic

--- game/JS/main.js
+++ game/JS/main.js
@@ -9,26 +9,48 @@ console.log('User Flipped ' + cardThree);
 */
 
 var cards = ['queen', 'queen', 'king', 'king'];
-var cardInPlay = [];
+var cardInPlay = [];//is only 2 cards at a time 
 
-var cardOne = cards[0];
-var cardTwo = cards[3];
-var cardThree = cards[2];
-var cardFour = cards[1];
-
-cardInPlay.push(cardOne);
-cardInPlay.push(cardTwo);
 /*
-cardInPlay.push(cardThree);
-cardInPlay.push(cardFour);
+var checkForMath = funtion () {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+}
 */
 
-if (cardInPlay.length === 2) {
-	cardInPlay[0] === cardInPlay[1];
-	alert("You found a match!");	
-} else {
-	alert("Sorry, try again!");
+
+var flipCard = function (cardId) { //to store all steps that should happen when the user flips a card.//
+	cardInPlay.push(cards[cardId]);
+
+		if (cardInPlay.length === 2) {
+
+			if (cardInPlay[0] === cardInPlay[1]) {
+			alert("You found a match!");	
+			} else {
+			alert("Sorry, try again!");
+		}
+	}
+	console.log("User flipped " + cards[cardId]);
 }
 
 
-//console.log('User Flipped ' + cardTwo);
+flipCard(0);
+flipCard(2);
+
+/*
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+*/
+
+
+
+
